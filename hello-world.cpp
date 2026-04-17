332e4df7f05c91482331a9c2b21d32a0f0409464
Sunny <k.heman0123@gmail.com>
2026-04-17 11:49:59 +0530

    adding c++ foler with basics file

---
+++ hello-world.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // This is the C++ equivalent of <stdio.h>
+
+int main() {
+    // std::cout is used for printing, similar to printf in C
+    // std::endl is used to print a new line, similar to \n
+    std::cout << "Hello, World!" << std::endl;
+    
+    return 0;
+}
